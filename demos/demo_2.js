7ba69c01e11c480922141c5c37465cf7dd90d1c1
lo-th <3dflashlo@gmail.com>
2014-11-10 15:44:13 +0100

    up

--- demos/demo_2.js
+++ demos/demo_2.js
@@ -4,6 +4,7 @@ var fs = [];
 var geos = {};
 var v3d = new V3D.View(110,60,500);
 var v = v3d;
+
 // add basic grid
 v.addGrid(400, 20, [0,0,0], [0,0,0]);
 
@@ -12,20 +13,23 @@ var c = new UI.Clock();
 
 renderLoop();
 
+function renderLoop(){
+    for(var i = 0; i<fs.length; i++){
+        if(i==0) c.set(v.unDegrees(fs[i].f.rotation));
+        fs[i].run();
+    }
+    v.render();
+    requestAnimationFrame( renderLoop );
+}
 
-var l = new THREE.PlaneBufferGeometry(1,1);
-l.applyMatrix(new THREE.Matrix4().makeTranslation(0.5,0,0));
-var staticLinkMat = new THREE.MeshBasicMaterial({color:0xF964A7});
-var firstLinkMat = new THREE.MeshBasicMaterial({color:0x43B8CC});
-var baseLinkMat = new THREE.MeshBasicMaterial({color:0x059BB5, transparent:true, opacity:0.5});
-var extraLinkMat = new THREE.MeshBasicMaterial({color:0x059BB5, transparent:true, opacity:0.25});
+// import sea3D object pack
 
-// import object pack
 var objName = "basic_op"; // version optimiser
 var pool = new SEA3D.Pool();
 pool.load( ['../models/'+objName+'.sea', '../models/serpent.sea'], initObject, 'buffer' );
 
 function initObject(){
+
     geos['p0'] = pool.geo(objName+'_point0');
     geos['p1'] = pool.geo(objName+'_point1');
     geos['p2'] = pool.geo(objName+'_point2');
@@ -35,21 +39,17 @@ function initObject(){
     geos['p6'] = pool.geo(objName+'_point6');
     geos['p7'] = pool.geo(objName+'_point7');
     geos['p8'] = pool.geo(objName+'_point8');
-
     geos['j0'] = pool.geo(objName+'_joint');
     geos['j1'] = pool.geo(objName+'_joint_1');
 
-    //-----
+    //----- serpent
 
     geos['c1'] = pool.geo('serpent_center_high');
     geos['c2'] = pool.geo('serpent_center_low');
-
     geos['s1'] = pool.geo('serpent_high_rat');
     geos['s2'] = pool.geo('serpent_low_rat');
-
     geos['h1'] = pool.geo('serpent_high_norm');
     geos['h2'] = pool.geo('serpent_low_norm');
-
     geos['head'] = pool.geo('serpent_head');
     geos['end'] = pool.geo('serpent_end');
     geos['tongue'] = pool.geo('serpent_tongue');
@@ -60,15 +60,18 @@ function initObject(){
     for(var i = 0; i<48; i++){
         fs[i] = new formula(240-(i*20), i*(10*V3D.ToRad), true, false, i);
     }
+
 }
 
 
 
 
+// -------------------------------------------------
 
 
 // basic class 
 var formula = function(pz, r, link, label, num){
+
     label = label || false;
     this.mul = 10;
     this.pz = pz || 0;
@@ -80,6 +83,7 @@ var formula = function(pz, r, link, label, num){
     this.mesh.position.z = pz;
     // init formula class
     this.f = new Turbulence.Formula();
+    this.nLength = this.f.pNames.length;
     // the start rotation
     this.f.rotation = r || 0;
     this.labels = [];
@@ -90,12 +94,17 @@ var formula = function(pz, r, link, label, num){
     this.snakeLink = [];
     this.head = null;
 
+    // extra scale snake
+    var ex = (num/100);
+    if (num==0) ex = (num/100)+0.25;
+    if (num==1) ex = (num/100)+0.20;
+    if (num==2) ex = (num/100)+0.15;
+    if (num==3) ex = (num/100)+0.10;
+    if (num==4) ex = (num/100)+0.05;
 
-   // this.o = new V3D.Particle(this.mesh, this.f.pNames.length);
 
-    //console.log(this.f.pNames.length)
+
     // add each formula point to 3d view
-    this.nLength = this.f.pNames.length;
     var name;
     for(var i = 0; i<this.nLength; i++){
 
@@ -108,18 +117,14 @@ var formula = function(pz, r, link, label, num){
         if(name == 'y5'|| name == 'o3')this.pointsDecal[i] = -7*0.25;
         if(name == 'b4'){  this.linksDecal[i] =(-14*0.25); }
         if(name == 'y3'|| name == 'o1' || name == 'y1') this.linksDecal[i] =(-7*0.25);
-
         if(name == 'b3' ) this.linksDecal[i] =(-14*0.25);
         if(name == 'a1') this.linksDecal[i] =(-21*0.25);
         if(name == 'a2') this.linksDecal[i] =(-28*0.25);
-      //  if(name == 'o3')this.linksDecal[i] = -7*0.25;
-
         if(name != 'y4' && name != 'o4' && name != 'y5')this.points[i] = this.createPoint(name);
         
         if(link){ 
         	if(name == 'y4'){
-                this.links[i] = this.createLink(name, this.f.sizer[i], (this.f.sizer[i]/5)-((num/100)-0.2)); 
-                //this.links[i].translateX(-this.f.sizer[i]/5);
+                this.links[i] = this.createLink(name, this.f.sizer[i], (this.f.sizer[i]/5)-(ex-0.2));
             }
             else if(name!='y5' && name!='o4') this.links[i] = this.createLink(name, this.f.sizer[i]);
             else this.links[i]=0
@@ -135,7 +140,7 @@ var formula = function(pz, r, link, label, num){
         this.linksDecal.push(-14*0.25);
     	this.links.push(this.createLink('',this.f.sizer[this.nLength+1]));
         this.linksDecal.push(7*0.25);
-    	this.links.push(this.createLink('',(this.f.sizer[this.nLength+2]/3)+((num/100)+0.2)));
+    	this.links.push(this.createLink('',(this.f.sizer[this.nLength+2]/3)+(ex+0.2)));
         this.linksDecal.push(0*0.25);
     }
 
@@ -146,8 +151,8 @@ var formula = function(pz, r, link, label, num){
     //this.snakeLink[0] = this.createSnakeLink('high_rat', n);
     //this.snakeLink[1] = this.createSnakeLink('low_rat', n);
 
-    this.snakeLink[0] = this.createSnakeLink('high_norm', n, 1-(num/100));
-    this.snakeLink[1] = this.createSnakeLink('low_norm', n, 1-(num/100));
+    this.snakeLink[0] = this.createSnakeLink('high_norm', n, 1-ex);
+    this.snakeLink[1] = this.createSnakeLink('low_norm', n, 1-ex);
 
     //this.points.y4.visible = false;
 }
@@ -164,7 +169,10 @@ formula.prototype = {
                 this.snakeLink[0].position.set(p.x*this.mul, p.y*this.mul,this.pointsDecal[i]);
                 this.snakeLink[0].rotation.z = p.r-(Math.PI/2); 
                 this.snakeLink[1].position.set(p.x*this.mul, p.y*this.mul,this.pointsDecal[i]);
-                if(this.head!=null) this.head.rotation.z = ((p.r-(Math.PI/2)-(40*V3D.ToRad))/2)-Math.PI/2;
+                if(this.head!=null){
+                    this.head.position.set(p.x*this.mul, p.y*this.mul,this.pointsDecal[i]);
+                    this.head.rotation.z = -(p.r+(Math.PI/2))/2-(20*V3D.ToRad)//((p.r-(Math.PI/2)-(40*V3D.ToRad))/2)-Math.PI/2;
+                }
             }else if(name=='y5'){
                 this.snakeLink[1].rotation.z = p.r-(75*V3D.ToRad)
             }else if(name!='o4'){
@@ -243,16 +251,16 @@ formula.prototype = {
     	var m1, m2, m3, m4;
         if(type=='high_rat'){
             t = 1;
-            m1 = new THREE.Mesh(geos['c1'], v.mats.c7);
-            m2 = new THREE.Mesh(geos['s1'], v.mats.c7);
+            m1 = new THREE.Mesh(geos['c1'], v.mats.c4);
+            m2 = new THREE.Mesh(geos['s1'], v.mats.c4);
         } else if(type=='low_rat'){
             t = 2;
             m1 = new THREE.Mesh(geos['c2'], v.mats.c1);
             m2 = new THREE.Mesh(geos['s2'], v.mats.c1);
         } else if(type=='high_norm'){
             t = 1;
-            m1 = new THREE.Mesh(geos['c1'], v.mats.c7);
-            m2 = new THREE.Mesh(geos['h1'], v.mats.c7);
+            m1 = new THREE.Mesh(geos['c1'], v.mats.c4);
+            m2 = new THREE.Mesh(geos['h1'], v.mats.c4);
         } else if(type=='low_norm'){
             t = 2;
             m1 = new THREE.Mesh(geos['c2'], v.mats.c1);
@@ -262,14 +270,14 @@ formula.prototype = {
         if(n==1 && t==1){
             m4 = new THREE.Mesh(geos['tongue'], v.mats.c1);
             m3 = new THREE.Mesh(geos['head'], v.mats.c7);
-            m3.rotation.y = Math.PI
-            m3.rotation.z = 25*V3D.ToRad;
+            m3.rotation.y = Math.PI;
+            //m3.rotation.z = 25*V3D.ToRad;
             this.head = m3;
             m3.add(m4);
-            m.add(m3);
+            this.mesh.add(m3);
         }else if(n==2 && t==2){
             m3 = new THREE.Mesh(geos['end'], v.mats.c1);
-            m3.rotation.y = Math.PI
+            m3.rotation.y = Math.PI;
             m.add(m3);
         }
         m.add(m1);
@@ -282,11 +290,3 @@ formula.prototype = {
 }
 
 
-function renderLoop(){
-    for(var i = 0; i<fs.length; i++){
-        if(i==0) c.set(v.unDegrees(fs[i].f.rotation));
-        fs[i].run();
-    }
-    v.render();
-    requestAnimationFrame( renderLoop );
-}
